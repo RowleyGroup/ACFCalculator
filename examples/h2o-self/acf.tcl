bd22880c26a1864ca0417ba01779f8ca62f33845
Christopher Rowley <crowley@rock.mun.ca>
2016-07-26 15:05:17 -0230

    added examples for calculate of self-diffusion coefficient of h2o in water

---
+++ examples/h2o-self/acf.tcl
@@ -0,0 +1,23 @@
+colvarsTrajFrequency 1
+
+colvar {
+    name solute1
+    distanceZ {
+        main {
+            atomnumbers { 1 2 3 }
+        }
+
+        ref {
+            dummyAtom ( 0.000, 0.000, 0.000 )
+        }
+
+        axis (0.0, 0.0, 1.0)
+    }
+}
+
+harmonic {
+    name soluterest1
+    colvars  solute1
+    centers 0.0
+    forceConstant { 10 }
+}
